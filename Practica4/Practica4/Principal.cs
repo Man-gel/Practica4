e9cdb68be1524e9110a95e399a9b5fa39eb55d44
javieroropeza <night_captain@hotmail.com>
2014-10-29 15:45:14 -0600

    Base del proyecto.

---
+++ Practica4/Practica4/Principal.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Usuario
+ * Date: 29/10/2014
+ * Time: 03:43 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Practica4
+{
+	/// <summary>
+	/// Description of Principal.
+	/// </summary>
+	public class Principal
+	{
+		public static void Main (String[] args)
+		{
+			Console.WriteLine("Hola Mundo!");
+			Console.ReadKey(true);
+		}
+	}
+}
